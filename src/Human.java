b600654d816ee6063e4d5713b5b474a12cd36eca
afevralevtotyt <afevraleff@ya.ru>
2022-10-19 14:42:13 +0200

    Done

--- src/Human.java
+++ src/Human.java
@@ -1,14 +1,24 @@
+import java.time.Year;
+
 public class Human {
     String name;
     int yearOfBirth;
     String town;
     String job;
-
-    public Human(String name, int yearOfBirth, String town) {
-        this.name = name;
-        this.yearOfBirth = yearOfBirth;
-        this.town = town;
+    public Human(int yearOfBirth) {
+        this("Информация не указана", yearOfBirth, "Информация не указана", "Информация не указана");
     }
+    public Human(String name, int yearOfBirth, String town, String job) {
+        this.name = name;
+        if (yearOfBirth>0){
+        this.yearOfBirth = yearOfBirth;}
+        else {
+            this.yearOfBirth =0;
+        }
+        this.town = town;
+        this.job=job;
+    }
+
     void printHello(){
         System.out.println("Привет! Меня зовут "+ name +". Я из города "+ town + ". Я родился в "+yearOfBirth+ " году. Я работаю на должности "+ job+ ". Будем знакомы!");
 
